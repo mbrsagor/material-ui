6ddaadd4d8e42b15c5dac4608e7147d68597ffd6
Sagor <mbrsagor@gmail.com>
2020-10-29 16:56:10 +0600

    Create sidebar

---
+++ src/app/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import TextField from '@material-ui/core/TextField';
+import '../style/style.scss';
+import SideMenu from '../components/SideMenu';
+
+class App extends Component {
+  
+  render() { 
+    return (
+      <>
+        <SideMenu />
+      </>
+    );
+  }
+}
+ 
+export default App;
