0b9f78c024adb1bc3a7b96643669ec04d98a618e
Sagor <mbrsagor@gmail.com>
2020-10-29 15:38:32 +0600

    Metriel UI implement

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import TextField from '@material-ui/core/TextField';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  render() { 
+    return (
+      <>
+        <form noValidate>
+        <TextField
+          id="date"
+          label="Birthday"
+          type="date"
+          defaultValue="2017-05-24"
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+      </form>
+      </>
+    );
+  }
 }
-
+ 
 export default App;
