323040846a17943495bb8f9cdac316626d6f5705
mbrsagor <mbrsagor@gmail.com>
2021-03-05 20:11:42 +0600

    Refactor

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './style/style.scss';
+
+class App extends Component {
+  
+  render() { 
+    return (
+      <>
+        <h2>Hello world</h2>
+      </>
+    );
+  }
+}
+ 
+export default App;
--- src/components/SideMenu.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function SideMenu() {
-    return (
-        <div className="side_menu">
-            
-        </div>
-    )
-}
